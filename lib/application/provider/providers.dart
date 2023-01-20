4eb1e906c0d06d5a56d3d90616be89dd43236c55
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2023-01-20 22:43:02 +0500

    remove unnecessary provider

--- lib/application/provider/providers.dart
+++ lib/application/provider/providers.dart
@@ -2,16 +2,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../core/utils/supabase_constant.dart';
 import '../models/user/user.dart';
-import 'user/user_notifier.dart';
 
 final userSupabaseStreamProvider = StreamProvider(
   (ref) => supabase.auth.onAuthStateChange,
-);
-
-final userNotifierProvider = StateNotifierProvider<UserNotifier, User>(
-  (ref) => UserNotifier(
-    ref.watch(userSupabaseStreamProvider).asData?.value.session?.user.id ?? "",
-  ),
 );
 
 final userSupabaseProvider = FutureProvider.autoDispose(
